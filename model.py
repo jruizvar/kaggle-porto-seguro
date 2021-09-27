efd26b557794d5feaa3fa1ae539842bec87f4625
Jose Cupertino Ruiz Vargas <jcruizva@hotmail.com>
2021-09-27 23:07:25 +0000

    Improve docstrings

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from sklearn.impute import MissingIndicator, SimpleImputer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import KBinsDiscretizer, MaxAbsScaler
+from sklearn.preprocessing import KBinsDiscretizer
 import numpy as np
 import os
 import pandas as pd
@@ -16,48 +16,35 @@ class Model:
 
         Parameters
         ----------
-        version: int
-            Supported versions are 1 or 2
+        n_features: int
+            Número de variáveis a serem exploradas.
 
         n_bins: int
-            Controls de discretization granularity
+            Número de bins na discretização de variáveis.
     """
-    def __init__(self, version, n_bins):
-        self.version = version
+    def __init__(self, n_features, n_bins):
+        self.n_features = n_features
         self.n_bins = n_bins
-        self.cols_ = ['var1']
-        self.score_ = 0.3
-        self.estimator_ = None
+        self._columns = ['var1']
+        self._score = 0.
+        self._estimator = None
 
-    def dataprep(self):
-        """ Define diferentes pipelines de preparação dos dados
-            segundo o número de versão.
-        """
-        if self.version == 1:
-            return make_pipeline(
-                SimpleImputer(strategy='median'),
-                MaxAbsScaler(),
-            )
-        elif self.version == 2:
-            return make_pipeline(
-                SimpleImputer(strategy='median'),
-                KBinsDiscretizer(n_bins=self.n_bins, strategy='uniform'),
-            )
-        else:
-            return 'drop'
-
-    def model(self):
+    def _model(self):
         """ Método para definir o pipeline com as etapas
             de dataprep, over sampling, e algoritmo.
 
             Return
             ------
-            pipeline: function
+            _pipeline: function
         """
-        def pipeline(cols):
+        def _pipeline(columns):
+            dataprep = make_pipeline(
+                SimpleImputer(strategy='median'),
+                KBinsDiscretizer(n_bins=self.n_bins, strategy='uniform'),
+            )
             transformer = make_column_transformer(
-                (MissingIndicator(), cols),
-                (self.dataprep(), cols),
+                (MissingIndicator(), columns),
+                (dataprep, columns),
             )
             kargs = {
                 'C': 0.1,
@@ -69,36 +56,47 @@ class Model:
                 SMOTE(random_state=42),
                 LogisticRegression(**kargs),
             )
-        return pipeline
+        return _pipeline
 
-    def evaluate(self, X, y, cols):
-        """ Executa o modelo na lista cols e retorna o score.
+    def _evaluate(self, X, y, columns):
+        """ Avalia o modelo usando as colunas selecionadas
+            e retorna o F1-score.
+
+            Parameters
+            ----------
+            X: pd.DataFrame
+            y: pd.Series
+            columns: List[str]
 
             Return
             ------
             score: float
         """
-        pipeline = self.model()
-        clf = pipeline(cols)
+        pipeline = self._model()
+        clf = pipeline(columns)
         scores = cross_val_score(clf, X, y, scoring='f1', cv=15)
         score = np.mean(scores)
         return score
 
-    def iteratecolumns(self, X, y, ncol=2):
+    def _iteratecolumns(self, X, y, n=2):
         """ Método recursivo para encontrar as melhores colunas.
+
+            Parameters
+            ----------
+            X: pd.DataFrame
+            y: pd.Series
+            n: int
         """
-        print(f'score at iteration {ncol-1}: {self.score_:.5f}')
-        max_iterations = 20
-        if ncol > max_iterations:
-            return self.cols_
+        print(f'F1-score na iteração {n-1}: {self._score:.5f}')
+        if n > self.n_features:
+            return self._columns
         else:
-            testcol = f'var{ncol}'
-            testcols = self.cols_ + [testcol]
-            testscore = self.evaluate(X, y, testcols)
-            if testscore > self.score_:
-                self.cols_ = testcols
-                self.score_ = testscore
-            return self.iteratecolumns(X, y, ncol+1)
+            testcols = self._columns + [f'var{n}']
+            testscore = self._evaluate(X, y, testcols)
+            if testscore > self._score:
+                self._columns = testcols
+                self._score = testscore
+            return self._iteratecolumns(X, y, n+1)
 
     def train(self, X, y):
         """ Método para treinar o modelo.
@@ -108,15 +106,15 @@ class Model:
             X: pd.DataFrame
             y: pd.Series
         """
-        cols = self.iteratecolumns(X, y)
-        pipeline = self.model()
+        cols = self._iteratecolumns(X, y)
+        pipeline = self._model()
         clf = pipeline(cols)
         clf.fit(X, y)
-        self.estimator_ = clf
+        self._estimator = clf
 
     def submit(self, X_test, outfile, upload=False):
         """ Método para criar arquivo de submissão
-            conforme a sintaxe do Kaggle.
+            conforme a formatação do Kaggle.
 
             Parameters
             ----------
@@ -124,7 +122,7 @@ class Model:
             outfile: str
             upload: bool
         """
-        clf = self.estimator_
+        clf = self._estimator
         y_pred = clf.predict(X_test)
         s = pd.Series(y_pred, index=X_test.index, name='predicted')
         s.to_csv(outfile)
